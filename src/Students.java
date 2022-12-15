111c1ccac56d33b4d0e28420f6b5eb4ea2ff014a
Jxnks <68285391+Jxnks@users.noreply.github.com>
2022-12-15 09:48:35 +0000

    Initial commit

---
+++ src/Students.java
@@ -0,0 +1,10 @@
+public class Students {
+    final String name;
+    final String age;
+
+
+    public Students(String name, String age) {
+        this.name = name;
+        this.age = age;
+    }
+}
